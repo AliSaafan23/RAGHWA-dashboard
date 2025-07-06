b23737688b0c0cbfa3ce0fb25855a8ee36ece530
Your Name <you@example.com>
2025-07-06 10:41:26 +0300

    refactored tab bar to seperated component

---
+++ src/components/TabBarComponent.jsx
@@ -0,0 +1,27 @@
+
+import { Box, Tab } from '@mui/material';
+import React from 'react';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+
+export default function TabBarComponent({tabs,tabPanels}) {
+    const [value, setValue] = React.useState('1');
+    const handleChange = (event,newValue) => {
+        setValue(newValue);
+    }
+  return (
+    console.log("tabs",tabs,'tab panels',tabPanels),
+    <TabContext value={value}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <TabList onChange={handleChange}>
+        {  tabs.map((tab) => (
+            <Tab label={tab.label} value={tab.value} />))}
+        </TabList>
+      </Box>
+     {tabPanels.map((panel) => (
+         <TabPanel value={panel.value} key={panel.value}>
+           {panel.component}
+         </TabPanel>
+     ))}
+    </TabContext>
+  )
+}
